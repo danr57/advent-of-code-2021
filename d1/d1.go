30e5908a0c09d8bca79981d7cbe92095104bcb1e
Dan Ryan <dryan.dev@gmail.com>
2022-01-21 15:57:52 +0000

    refactoring to packages

--- d1/d1.go
+++ d1/d1.go
@@ -1,5 +1,28 @@
 package d1
 
-func Part1() int {
-	return 1
+import (
+	"log"
+	"strconv"
+)
+
+var (
+	count int = 0
+	x     int = 0
+)
+
+func Part1(s []string) int {
+	for _, n := range s {
+
+		t, err := strconv.Atoi(n)
+		if err != nil {
+			log.Fatal("Error converting strings to int:", err)
+		}
+
+		if t > x {
+			count++
+		}
+		x = t
+	}
+
+	return count
 }
--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
 
 	"github.com/danr57/advent-of-code-2022/d1"
 )
 
 func main() {
-	fmt.Println(d1.Part1())
+	log.Println("Day 1")
+	d1input := readAndSplit("d1/input")
+	log.Println(d1.Part1(d1input))
+
+}
+
+func readAndSplit(s string) []string {
+	i, err := ioutil.ReadFile(s)
+
+	if err != nil {
+		log.Fatal("Error reading input file:", err)
+	}
+
+	return strings.Split(string(i), "\n")
+
 }
