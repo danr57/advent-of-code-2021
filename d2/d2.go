803c1978120a99084ffcab712299d4f70ac8aa48
Dan Ryan <dryan.dev@gmail.com>
2022-01-23 02:42:54 +0000

    Day 2 MVP done, d2p2 test not met

--- d2/d2.go
+++ d2/d2.go
@@ -1,9 +1,70 @@
 package d2
 
-func Part1([]string) int {
-	return 150
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+func Part1(route []string) int {
+	x := 0
+	y := 0
+
+	for _, d := range route {
+		t := strings.Split(string(d), " ")
+		n, _ := strconv.Atoi(t[1])
+		switch t[0] {
+		case "up":
+			y -= n
+		case "down":
+			y += n
+		case "forward":
+			x += n
+		case "backward":
+			x -= n
+		}
+	}
+	return x * y
 }
 
-func Part2([]string) int {
-	return 900
+func Part2(route []string) int {
+	a := 0
+	x := 0
+	y := 0
+
+	for _, d := range route {
+		t := strings.Split(string(d), " ")
+		n, _ := strconv.Atoi(t[1])
+		switch t[0] {
+		case "up":
+			a -= n
+		case "down":
+			a += n
+		case "forward":
+			x += n
+			y -= (a * n)
+		case "backward":
+			x -= n
+		}
+	}
+
+	// TODO: This is returning the answer as a negative int
+	// TODO: Problem demands a positive result
+	return x * y
+}
+
+// TODO: Process input to a better structure
+func ReadAndSplit(r string) []string {
+	input, err := os.ReadFile(filepath.Clean(r))
+	if err != nil {
+		log.Fatalf("Error reading file \"%v\": %v", r, err)
+	}
+
+	if len(input) < 1 {
+		log.Fatalf("Data input empty. Please populate")
+	}
+
+	return strings.Split(string(input), "\n")
 }
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/danr57/advent-of-code-2022/d1"
+	"github.com/danr57/advent-of-code-2022/d2"
 )
 
 func main() {
@@ -11,4 +12,6 @@ func main() {
 	d1input := d1.ReadAndSplit("d1/input")
 	log.Printf("===Day 1=== \nPart 1: %v \nPart 2: %v", d1.Part1(d1input), d1.Part2(d1input))
 
+	d2input := d2.ReadAndSplit("d2/input")
+	log.Printf("===Day 2=== \nPart 1: %v \nPart 2: %v", d2.Part1(d2input), d2.Part2(d2input))
 }
