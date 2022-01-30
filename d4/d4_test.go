24082918d04d281b9e31aedfd7c9fb972b7723d7
Dan Ryan <dryan.dev@gmail.com>
2022-01-30 13:19:55 +0000

    test: tests prepared

--- d4/d4_test.go
+++ d4/d4_test.go
@@ -1,6 +1,7 @@
 package d4_test
 
 import (
+	"log"
 	"testing"
 
 	"github.com/danr57/advent-of-code-2022/d4"
@@ -9,6 +10,8 @@ import (
 var (
 	ans1 = 4512
 	ans2 = 0
+	sol1 = 23177
+	sol2 = 6804
 )
 
 //TODO: Write tests for actual results
@@ -19,10 +22,16 @@ func TestPart1(t *testing.T) {
 	t.Run("Day 4.1: Sample", func(t *testing.T) {
 		t.Parallel()
 
-		r := d4.Part1()
-
-		if r != ans1 {
+		if r := d4.Part1(); r != ans1 {
 			t.Fatalf("Expected: %v. Got: %v", ans1, r)
+		}
+	})
+
+	t.Run("Day4.1: Actual: ", func(t *testing.T) {
+		t.Parallel()
+
+		if r := d4.Part1(); r != sol1 {
+			log.Fatalf("Expected: %v. Got: %v.", sol1, r)
 		}
 	})
 }
@@ -33,10 +42,16 @@ func TestPart2(t *testing.T) {
 	t.Run("Day 4.2: Sample", func(t *testing.T) {
 		t.Parallel()
 
-		r := d4.Part2()
-
-		if r != ans2 {
+		if r := d4.Part2(); r != ans2 {
 			t.Fatalf("Expected: %v. Got: %v", ans2, r)
+		}
+	})
+
+	t.Run("Day 4.2: Actual: ", func(t *testing.T) {
+		t.Parallel()
+
+		if r := d4.Part2(); r != sol2 {
+			log.Fatalf("Expected: %v. Got: %v.", sol2, r)
 		}
 	})
 }
